3838c20e0ed63819cfa513b893ac9027bd8b2445
Sandeep Kumar <kumarsandeep110104@gmail.com>
2025-08-27 15:20:06 +0530

    resolved the tailwind issue

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 TAILWIND_APP_NAME = "theme"
-INTERNAL_IPS = [127.0.0.1]
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
